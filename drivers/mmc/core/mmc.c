9948eaad274888979e9c02646a66e044d7f917ab
Ruben <ruben@novation.be>
2017-03-11 17:41:26 +0100

    Removed revision check for mmc as this breaks forward compatibility with newer mmc revisions
    Change-Id: Ibb41334ec64e24a2ca0bf28112774466137579d3

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -357,12 +357,6 @@ static int mmc_read_ext_csd(struct mmc_card *card, u8 *ext_csd)
 	}
 
 	card->ext_csd.rev = ext_csd[EXT_CSD_REV];
-	if (card->ext_csd.rev > 7) {
-		pr_err("%s: unrecognised EXT_CSD revision %d\n",
-			mmc_hostname(card->host), card->ext_csd.rev);
-		err = -EINVAL;
-		goto out;
-	}
 
 	/* fixup device after ext_csd revision field is updated */
 	mmc_fixup_device(card, mmc_fixups);
